42d8abb80a65f5a4439cbe343d7f7c07823f7eda
wally-jnyor <arealunar9@gmail.com>
2023-12-18 23:12:44 -0300

    update fake database

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="main">
     <div class="container" v-for="user in userData" :key="user.id">
+
       <div class="title">
-        <h2>Hello, {{user.name}}! - {{user.dataHora}}</h2>
+        <h2>Hello, {{ user.name }}! - {{ user.dataHora }}</h2>
         <div v-for="finance in user.finance.investment_cash" :key="finance.id">
-          <p>Target: {{ finance.name }} - Progress: {{ finance.total }} / {{finance.amount}}</p>
+          <p>Target: {{ finance.name }} - Progress: {{ finance.total }} / {{ finance.amount }}</p>
+          <p>fakeback- {{!finance ? userFakeBackend : finance.name}}</p>
         </div>
-        <p >something motivating</p>
+        <p>something motivating</p>
         <div class="navbar">
           <li class="navigationList">
             <RouterLink to="/user-details">Details</RouterLink>
           </li>
         </div>
       </div>
+
       <div class="title">
         <h2>Missions in process</h2>
         <div v-for="task in user.checklist" :key="task.id">
-          <p>Doing now: {{task.title}} - {{ task.dataHora }} - {{ task.local }}</p>
+          <p>Doing now: {{ task.title }} - {{ task.dataHora }} - {{ task.local }}</p>
         </div>
         <div class="navbar">
           <li class="navigationList">
@@ -28,7 +31,7 @@
         <h2>Completed</h2>
         <p>something motivating</p>
         <div v-for="task in user.checklist" :key="task.id">
-          <p v-if="task.status != 1">{{task.title}} - {{ task.dataHora }} - {{ task.local }}</p>
+          <p v-if="task.status != 1">{{ task.title }} - {{ task.dataHora }} - {{ task.local }}</p>
         </div>
         <div class="navbar">
           <li class="navigationList">
@@ -41,25 +44,32 @@
 </template>
 
 <script>
-//import userData from '../fakeBackend/UserData'
-import axios from 'axios';
+import fakeBackEnd from '../fakeBackend/UserData' //mobile acess
+import axios from 'axios'
 
 export default {
   data() {
     return {
       userData: [],
-    };
+      userFakeBackend: fakeBackEnd
+    }
   },
   mounted() {
-    axios.get('http://localhost:3000/userData')
-      .then(response => {
-        this.userData = response.data;
-      })
-      .catch(error => {
-        console.error('Erro ao buscar dados:', error);
-      });
+    this.getUserData()
   },
-};
+  methods: {
+    getUserData() {
+      axios
+        .get('http://localhost:3000/userData')
+        .then((response) => {
+          this.userData = response.data
+        })
+        .catch((error) => {
+          console.error('Erro ao buscar dados:', error)
+        })
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -92,9 +102,10 @@ export default {
     justify-content: space-between;
     padding: 10px;
     background-color: #ffaaf052;
-    border-radius: 25PX;
+    border-radius: 25px;
   }
   .title {
+    color: #ffff;
   }
 }
 </style>
