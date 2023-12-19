e8b27adc8a1268505e0fc20494be34ae9224795e
wally-jnyor <arealunar9@gmail.com>
2023-12-19 01:23:50 -0300

    update fake database

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,48 +5,7 @@
       <div v-for="user in userData" :key="user.id">
         <div class="title">
         <h3>has acess to desktop</h3>
-        <h2>Hello, {{ user.name }}! - {{ user.dataHora }}</h2>
-        <div v-for="finance in user.finance.investment_cash" :key="finance.id">
-          <p>Target: {{ finance.name }} - Progress: {{ finance.total }} / {{ finance.amount }}</p>
-        </div>
-        <p>something motivating</p>
-        <div class="navbar">
-          <li class="navigationList">
-            <RouterLink to="/user-details">Details</RouterLink>
-          </li>
-        </div>
-      </div>
-
-      <div class="title">
-        <h2>Missions in process</h2>
-        <div v-for="task in user.checklist" :key="task.id">
-          <p>Doing now: {{ task.title }} - {{ task.dataHora }} - {{ task.local }}</p>
-        </div>
-        <div class="navbar">
-          <li class="navigationList">
-            <RouterLink to="/quests">Details</RouterLink>
-          </li>
-        </div>
-      </div>
-      <div class="title">
-        <h2>Completed</h2>
-        <p>something motivating</p>
-        <div v-for="task in user.checklist" :key="task.id">
-          <p v-if="task.status != 1">{{ task.title }} - {{ task.dataHora }} - {{ task.local }}</p>
-        </div>
-        <div class="navbar">
-          <li class="navigationList">
-            <RouterLink to="/bag">Details</RouterLink>
-          </li>
-        </div>
-      </div>
-      </div>
-    </div>
-
-    <div class="container" v-show="!hasAcessToDesktop">
-      <div v-for="user in userDatabackend" :key="user.id">
-        <div class="title">
-        <h2>Hello, {{ user.name }}! - {{ user.dataHora }}</h2>
+        <h2>Hello, {{ user.name || userDatabackend.name}}! - {{ user.dataHora }}</h2>
         <div v-for="finance in user.finance.investment_cash" :key="finance.id">
           <p>Target: {{ finance.name }} - Progress: {{ finance.total }} / {{ finance.amount }}</p>
         </div>
