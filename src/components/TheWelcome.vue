97ddf3e858b6af0bde60bf75a64df1685fe2a623
wally-jnyor <arealunar9@gmail.com>
2023-12-18 23:56:42 -0300

    update fake database

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,46 +1,11 @@
 <template>
   <div class="main">
     <div class="container" v-for="user in userData" :key="user.id">
+<p>fakeback- {{userFakeBackend}}</p>
 
-      <div class="title">
-        <h2>Hello, {{ user.name }}! - {{ user.dataHora }}</h2>
-        <div v-for="finance in user.finance.investment_cash" :key="finance.id">
-          <p>Target: {{ finance.name }} - Progress: {{ finance.total }} / {{ finance.amount }}</p>
-          <p>fakeback- {{userFakeBackend}}</p>
-        </div>
-        <p>something motivating</p>
-        <div class="navbar">
-          <li class="navigationList">
-            <RouterLink to="/user-details">Details</RouterLink>
-          </li>
-        </div>
-      </div>
-
-      <div class="title">
-        <h2>Missions in process</h2>
-        <div v-for="task in user.checklist" :key="task.id">
-          <p>Doing now: {{ task.title }} - {{ task.dataHora }} - {{ task.local }}</p>
-        </div>
-        <div class="navbar">
-          <li class="navigationList">
-            <RouterLink to="/quests">Details</RouterLink>
-          </li>
-        </div>
-      </div>
-      <div class="title">
-        <h2>Completed</h2>
-        <p>something motivating</p>
-        <div v-for="task in user.checklist" :key="task.id">
-          <p v-if="task.status != 1">{{ task.title }} - {{ task.dataHora }} - {{ task.local }}</p>
-        </div>
-        <div class="navbar">
-          <li class="navigationList">
-            <RouterLink to="/bag">Details</RouterLink>
-          </li>
-        </div>
       </div>
     </div>
-  </div>
+
 </template>
 
 <script>
@@ -67,7 +32,7 @@ export default {
         .catch((error) => {
           console.error('Erro ao buscar dados:', error)
         })
-    }
+    },
   }
 }
 </script>
--- src/fakeBackend/UserData.js
+++ src/fakeBackend/UserData.js
@@ -2,38 +2,63 @@
 
 const userData = [
     {
-        id: 1,
-        name: "Completar relatório",
-        age: 20,
-        dataHora: "2023-12-15T08:00:00",
-        prioridade: "alta",
-        ativo: true,
-        status: "pendente",
-        work: [
+        "id": 1,
+        "name": "João Silva",
+        "age": 30,
+        "dataHora": "2023-12-15T08:00:00",
+        "prioridade": "alta",
+        "ativo": true,
+        "status": "pendente",
+        "work": [
             {
-                occupation: "Revisar estatísticas",
-                wage: 2400
+                "occupation": "Revisar estatísticas",
+                "wage": 2400
             }
         ],
-        finance: {
-            wage: 2400,
-            safe: 400,
-            free_money: 300,
-            monthly_expenses: 500,
-            investment_cash: [
+        "finance": {
+            "wage": 2400,
+            "safe": 400,
+            "free_money": 300,
+            "monthly_expenses": 500,
+            "investment_cash": [
                 {
-                    name: "pc gamer",
-                    amount: 300,
-                    total: 127
+                    "id":2,
+                    "name": "pc gamer",
+                    "amount": 4000,
+                    "total": 127
                 },
-            ],
+                {
+                    "id":3,
+                    "name": "casa",
+                    "amount": 100000,
+                    "total": 20000
+                }
+            ]
         },
-        checklist: [
-            { item: "Revisar estatísticas", feito: true },
-            { item: "Adicionar gráficos", feito: false },
-            { item: "Finalizar conclusões", feito: false }
-        ],
-    },
-];
+        "checklist": [
+            {
+                "title": "Revisar estatísticas",
+                "description": "ir em tal lugar, fazer tal coisa",
+                "local" : "rua tal, loja tal, numero tal",
+                "dataHora": "2023-12-15T08:00:00",
+                "status": 1
+            },
+            {
+                "title": "Role não sei aonde",
+                "description": "ir em tal lugar, fazer tal coisa",
+                "local" : "rua tal, loja tal, numero tal",
+                "dataHora": "2023-12-15T08:00:00",
+                "status": 0
+            },
+            {
+                "title": "Organizar a casa",
+                "description": "ir em tal lugar, fazer tal coisa",
+                "local" : "rua tal, loja tal, numero tal",
+                "dataHora": "2023-12-15T08:00:00",
+                "status": 1
+            }
+        ]
+    }
+]
 
 export default userData;
